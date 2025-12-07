d21b1a154c1d2057317aaff4377ea929ecca0f94
Igor Lessa Robaina de Souza <igorleessa@gmail.com>
2025-12-07 14:02:00 -0300

    Correção para rodar local

--- frontend/mw-site/src/environments/environment.ts
+++ frontend/mw-site/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000',
+  apiUrl: 'http://localhost:5293',
   whatsappNumber: '5511999999999',
   companyEmail: 'contato@medicwarehouse.com',
   companyPhone: '(11) 99999-9999'
--- frontend/mw-system-admin/src/environments/environment.ts
+++ frontend/mw-system-admin/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'http://localhost:5293/api',
   enableDebug: true,
   useMockData: false, // Enable to use mocked data instead of real API calls
   security: {
